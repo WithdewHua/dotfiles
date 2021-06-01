8a3ab01e982b24fa8e32357c3d5bf928e9a1e08e
WithdewHua <i@10101.io>
2021-06-01 23:08:57 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -220,10 +220,16 @@ _config_shell() {
 
 # VIM
 config_vim() {
-    create_symlinks "vim/vimrc" ".vimrc"
-    type nvim > /dev/null 2>&1 && mkdir -p $HOME/.config/nvim && {
-        create_symlinks "vim/vimrc" ".config/nvim/init.vim"
-        create_symlinks "vim/coc-settings.json" ".config/nvim/coc-settings.json"
+    ask "Use vim/nvim with plug?"
+    [ $? -eq 1 ] && {
+        create_symlinks "vim/vimrc" ".vimrc"
+        type nvim > /dev/null 2>&1 && mkdir -p $HOME/.config/nvim && {
+            create_symlinks "vim/vimrc" ".config/nvim/init.vim"
+            create_symlinks "vim/coc-settings.json" ".config/nvim/coc-settings.json"
+        } || {
+            create_symlinks "vim/vimrc-basic" ".vimrc"
+            type nvim > /dev/null 2>&1 && mkdir -p $HOME/.config/nvim && create_symlinks "vim/vimrc-basic" ".config/nvim/init.vim"
+        }
     }
 }
 
