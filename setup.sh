040870fd7fbc7649165a97f7cf4b2bf05a26f79a
WithdewHua <i@10101.io>
2021-07-03 21:10:42 +0800

    fix setup: mkdir nvim

--- setup.sh
+++ setup.sh
@@ -236,10 +236,14 @@ config_vim() {
     type nvim > /dev/null 2>&1 && {
         ask "Use nvim with plug?"
         [ $? -eq 1 ] && {
+            mkdir -p $HOME/.config/nvim
             create_symlinks "vim/nvim/init.vim" ".config/nvim/init.vim"
             create_symlinks "vim/nvim/rc" ".config/nvim/rc"
             create_symlinks "vim/nvim/coc-settings.json" ".config/nvim/coc-settings.json"
-        } || create_symlinks "vim/vimrc" ".config/nvim/init.vim"
+        } || {
+            mkdir -p $HOME/.config/nvim
+            create_symlinks "vim/vimrc" ".config/nvim/init.vim"
+        }
     }
 }
 
