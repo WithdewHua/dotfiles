b9175b5c6710d78e9c0d88c471f0aba962085b31
WithdewHua <i@10101.io>
2021-07-25 12:42:17 +0800

    [nvim]-update autosession config

--- vim/nvim/lua/plugins/autosession.lua
+++ vim/nvim/lua/plugins/autosession.lua
@@ -15,7 +15,7 @@ autosession.setup{
     auto_session_enable_laste_session = false,
     auto_session_root_dir = vim.fn.stdpath('config').."/sessions/",
     auto_session_enabled = true,
-    auto_save_enabled = false,
+    auto_save_enabled = true,
     auto_restore_enabled = nil,
     auto_session_suppress_dirs = nil
 }
