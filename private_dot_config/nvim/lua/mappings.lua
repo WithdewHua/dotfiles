f04fa04cbf48573e5243628a1bf6cfcfe22240b1
WithdewHua <i@10101.io>
2021-12-31 00:28:56 +0800

    [nvim]-Update lsp config

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -71,8 +71,8 @@ map("", "<Up>", 'v:count ? "k" : "gk"', {expr = true})
 
 -- trouble
 map("n", "<leader>xx", "<cmd>Trouble<cr>", opt)
-map("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opt)
-map("n", "<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>", opt)
+map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opt)
+map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opt)
 map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opt)
 map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opt)
 map("n", "<leader>xr", "<cmd>Trouble lsp_references<cr>", opt)
--- private_dot_config/nvim/lua/plugins/_lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/_lspconfig.lua
@@ -70,14 +70,81 @@ vim.notify = function(msg, log_level, _opts)
 end
 
 -- lsp_installer
--- Register a handler that will be called for all installed servers.
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
+-- Include the servers you want to have installed by default below
+local servers = {
+    "pyright",
+    "yamlls",
+    "jsonls",
+    "sumneko_lua",
+    "bashls",
+}
 
+for _, name in pairs(servers) do
+    local server_is_found, server = lsp_installer.get_server(name)
+    if server_is_found then
+        if not server:is_installed() then
+            print("Installing " .. name)
+            server:install()
+        end
+    end
+end
+-- Register a handler that will be called for all installed servers.
+local function on_attach(client, bufnr)
+    -- Set up buffer-local keymaps
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+
+local enhance_server_opts = {
     -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+    ["pyright"] = function(opts)
+        opts.settings = {
+            python = {
+                analysis = {
+                    autoImportCompletions = false,
+                }
+            }
+        }
+    end
+
+}
+lsp_installer.on_server_ready(function(server)
+    -- Specify the default options which we'll use to setup all servers
+    local opts = {
+        on_attach = on_attach,
+    }
+
+    if enhance_server_opts[server.name] then
+        -- Enhance the default opts with the server-specific ones
+        enhance_server_opts[server.name](opts)
+    end
 
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
