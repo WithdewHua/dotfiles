c4e5d0bfc10899334f79ed89b23d1e370c3b4d86
WithdewHua <i@10101.io>
2022-01-16 16:00:54 +0800

    [nvim]-update folder

--- private_dot_config/nvim/lua/plugins/_pretty_fold.lua
+++ private_dot_config/nvim/lua/plugins/_pretty_fold.lua
@@ -25,7 +25,7 @@ pretty_fold.setup {
     -- "delete" : Delete all comment signs from the fold string.
     -- "spaces" : Replace all comment signs with equal number of spaces.
     -- false    : Do nothing with comment signs.
-    comment_signs = 'spaces',
+    process_comment_signs = 'spaces',
 
     -- List of patterns that will be removed from content foldtext section.
     stop_words = {
