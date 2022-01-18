0778037c76ec9b5dbffd7506648c158a4910f512
WithdewHua <i@10101.io>
2022-01-19 03:02:18 +0800

    [wezterm]-add configuration of wezterm

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+return {
+    -- font
+    font = wezterm.font("CaskaydiaCove Nerd Font"),
+    font_size = 12,
+    -- color scheme
+    color_scheme = "Gruvbox Dark",
+    -- scrollback
+    scrollback_lines = 10000,
+    enable_scroll_bar = true,
+}
