23d8d670addf12fd95db2cea12df87a64c7a397e
WithdewHua <i@10101.io>
2022-08-25 04:42:24 +0800

    [wezterm]-update config

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,10 +4,14 @@ return {
     -- font
     font = wezterm.font("CaskaydiaCove Nerd Font"),
     font_size = 12,
+    -- keyboard
+    use_dead_keys = false,
     -- color scheme
     color_scheme = "Gruvbox Dark",
     -- scrollback
     scrollback_lines = 10000,
     enable_scroll_bar = true,
     native_macos_fullscreen_mode = true,
+
+    audible_bell = "Disabled",
 }
