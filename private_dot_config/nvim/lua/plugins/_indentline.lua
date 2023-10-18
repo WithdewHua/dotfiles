319bc8cd3e94fa4935640acec9cba11c1c2b4a93
WithdewHua <i@10101.io>
2023-10-18 14:44:46 +0800

    fix(nvim): indent-blankline v3

--- private_dot_config/nvim/lua/plugins/_indentline.lua
+++ private_dot_config/nvim/lua/plugins/_indentline.lua
@@ -3,15 +3,9 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         event = "BufRead",
+        main = "ibl",
         config = function()
-            vim.g.indentLine_enabled = 1
-            vim.g.indent_blankline_char = "▏"
-
-            vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-            vim.g.indent_blankline_buftype_exclude = {"terminal"}
-
-            vim.g.indent_blankline_show_trailing_blankline_indent = false
-            vim.g.indent_blankline_show_first_indent_level = false
+            require("ibl").setup()
         end
     },
 }
