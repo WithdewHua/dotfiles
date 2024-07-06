c48d81d9766d00dee4268184981a4e3816ec4bd3
WithdewHua <i@10101.io>
2024-07-06 15:33:48 +0800

    feat: update app

--- dot_zsh/tools/poetry.zsh
+++
@@ -1,5 +0,0 @@
-pathappend $HOME/.poetry/bin
-
-mkdir -p ~/.zsh/completions
-poetry completions zsh > ~/.zsh/completions/_poetry
-
--- dot_zsh/tools/pyenv.zsh
+++
@@ -1,8 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-pathprepend ${PYENV_ROOT}/bin
-
-eval "$(pyenv init -)"
-
-eval "$(pyenv virtualenv-init -)"
-
-source $(pyenv root)/completions/pyenv.zsh
