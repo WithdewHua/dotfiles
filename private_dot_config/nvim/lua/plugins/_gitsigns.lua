fdf267e69577e2213b30f215eed1dd22de7181aa
WithdewHua <i@10101.io>
2024-08-02 14:40:41 +0800

    fix(nvim): gitsigns

--- private_dot_config/nvim/lua/plugins/_gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/_gitsigns.lua
@@ -4,16 +4,26 @@ return {
         event = "BufRead",
         opts = {
             signs = {
-                add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-                change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-                delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+                add          = { text = '┃' },
+                change       = { text = '┃' },
+                delete       = { text = '_' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
             },
+            signs_staged = {
+                add          = { text = '┃' },
+                change       = { text = '┃' },
+                delete       = { text = '_' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
+            },
+            signs_staged_enable = true,
             signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
             numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
             linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-            word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+            word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
             on_attach = function ()
                 local gs = package.loaded.gitsigns
 
@@ -66,9 +76,6 @@ return {
                 delay = 1000,
                 ignore_whitespace = false,
             },
-            current_line_blame_formatter_opts = {
-                relative_time = false
-            },
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
@@ -80,9 +87,6 @@ return {
                 relative = 'cursor',
                 row = 0,
                 col = 1
-            },
-            yadm = {
-                enable = false
             },
         },
         config = function(_, opts)
