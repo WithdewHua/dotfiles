064ddd4376ce8fb4b9a9b5d3708ec43af23359a2
WithdewHua <i@10101.io>
2025-02-08 03:27:59 +0800

    feat(nvim): enable snacks pickers

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- LSP
+-- Extras/lang
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+-- Extras/editor
+vim.g.lazyvim_picker = "snacks"
